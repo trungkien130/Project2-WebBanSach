27e1238884001f09fe8d740e90f94321a539d0ca
trungkien130 <kienlee14@gmail.com>
2024-11-07 15:34:15 +0700

    buoi2

---
+++ K22CNT4-LeTrungKien-Project2/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_thuc_tap_chuyen_de_1.Models;
+namespace Web_thuc_tap_chuyen_de_1.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ThucTapChuyenDe1Entities db = new ThucTapChuyenDe1Entities();
+
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            var model = new DashboardViewModel()
+            {
+                KhachHang = db.KhachHang.Count(),
+                DanhMucSach = db.DanhMucSach.Count(),
+                GioHang = db.Cart.Count(),
+                Sach = db.Sach.Count(),
+                Slide = db.Slide.Count()
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ K22CNT4-LeTrungKien-Project2/Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_thuc_tap_chuyen_de_1.Models
+{
+    public class DashboardViewModel
+    {
+        public int KhachHang { get; set; }
+        public int DanhMucSach { get; set; }
+        public int GioHang { get; set; }
+        public int Sach { get; set; }
+        public int Slide { get; set; }
+    }
+}
